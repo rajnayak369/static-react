56e46e766433dc3cd4e6f0421aa280ef470fff70
Sairaj Naik Guguloth <s.guguloth.22@abdn.ac.uk>
2024-03-23 17:22:58 +0000

    images folder added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,9 @@
+import logo from '../images/logo.png'
 
 function Navbar() {
     return (
         <nav>
-            <img src="../logo192.png" className="nav--icon" />
+            <img src={logo} alt="logo" className="nav--icon" />
             <h3 className="nav--logo_text">StaticReact</h3>
             <h4 className="nav--title">React - Project 1 <a href="https://www.linkedin.com/in/your-profile" className="nav-link">@sairaj_naik</a></h4>
         </nav>
