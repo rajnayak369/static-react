1f01f4e8e1ee0c46a479703e1b5aadd6d44d8a34
Sairaj Naik Guguloth <s.guguloth.22@abdn.ac.uk>
2024-03-23 19:08:11 +0000

    changes 2 for mobile

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -6,8 +6,8 @@ function MainContent() {
                 <ul className="main--facts">
                     <li>This is a simple static React application </li>
                     <li>Project aims is to learn the basic components of a React app</li>
-                    <li>Source can be found at -> <a href="https://github.com/rajnayak369/static-react" className="li-link">Link</a> </li>
-                    <li>Application is hosted using git hub pages -> <a href="https://rajnayak369.github.io/static-react/" className="li-link">Link</a></li>
+                    <li>Source can be found at &rarr; <a href="https://github.com/rajnayak369/static-react" className="li-link">Link</a> </li>
+                    <li>Application is hosted using git hub pages &rarr; <a href="https://rajnayak369.github.io/static-react/" className="li-link">Link</a></li>
                     <li>If you like you can reach out at g.sairajnaik@gmail.com</li>
                 </ul>
             </div>
